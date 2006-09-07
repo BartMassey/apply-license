51141cf083b08690f871614bf7b2939ff1e115f2
Bart Massey <bart@bart.cs.pdx.edu>
2006-09-06 20:20:37 -0700

    Used tool to apply license to itself

--- apply-license.sh
+++ apply-license.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright (c) 2006 Bart Massey
+# All Rights Reserved
+# Please see the end of this file for license information.
 
 # many temporary files are needed
 CFILE=/tmp/al-cfile.$$; TMPFILES="$CFILE"
@@ -122,3 +125,25 @@ fi
 
 # mark license as applied
 echo "License information applied by apply-license" > .apply-license
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the
+# Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall
+# be included in all copies or substantial portions of the
+# Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
