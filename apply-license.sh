e2fc93d2413757c06cf8c58e2ed29edd86870bac
Bart Massey <bart@cs.pdx.edu>
2009-01-01 00:33:50 -0800

    added TeX support

--- apply-license.sh
+++ apply-license.sh
@@ -217,6 +217,41 @@ then
   done
 fi
 
+# percent comments for TeX
+ls *.tex >/dev/null 2>&1
+if [ $? = 0 ]
+then
+  echo '1,$ s=^=% =' > $PTMP
+  sed -f $PTMP $CFILE > $CTMP
+  sed -f $PTMP $LFILE > $LTMP
+  sed -f $PTMP $SCFILE > $SCTMP
+  ls *.tex |
+  while read F
+  do
+    sed '1 !d; s/^% //; s/ .*$//' < $F | (
+      read WORD
+      if [ "$WORD" != Copyright ]
+      then
+	if $HEURISTIC
+	then
+	    if [ `wc -l $F | awk '{print $1;}'` -lt $LFILELEN2 ]
+	    then
+	       SHORT=true
+	    else
+	       SHORT=false
+	    fi
+	fi
+	if $SHORT
+	then
+	  $EDIT $F < $SSTMP
+	else
+	  $EDIT $F < $STMP
+	fi
+      fi
+    )
+  done
+fi
+
 # semi-semi comments for Emacs lisp and Common Lisp
 for suff in lisp el
 do
