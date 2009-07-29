5eed3efc1969803643ecd4a7bc4738f2bc3d870d
Bart Massey <bart@cs.pdx.edu>
2009-07-29 13:30:02 -0700

    added support for LaTeX sty and cls files and for m4

--- apply-license.sh
+++ apply-license.sh
@@ -183,6 +183,43 @@ then
   )
 fi
 
+# dnl comments for m4
+ls *.m4 >/dev/null 2>&1
+if [ $? = 0 ]
+then
+  echo '1,$ s=^=dnl =' > $PTMP
+  sed -f $PTMP $CFILE > $CTMP
+  sed -f $PTMP $LFILE > $LTMP
+  sed -f $PTMP $SCFILE > $SCTMP
+  ls *.m4 |
+  while read F
+  do
+    sed '1 !d; s/^dnl //; s/ .*$//' < $F | (
+      read WORD
+      if [ "$WORD" != Copyright ]
+      then
+	if $HEURISTIC
+	then
+	    if [ `wc -l $F | awk '{print $1;}'` -lt $LFILELEN2 ]
+	    then
+	       SHORT=true
+	    else
+	       SHORT=false
+	    fi
+	fi
+	if $SHORT
+	then
+	  $EDIT $F < $SSTMP
+	  echo "1,/^$/s/^$/dnl/" | $EDIT $F
+	else
+	  $EDIT $F < $STMP
+	  echo "1,/^$/s/^$/dnl/" | $EDIT $F
+	fi
+      fi
+    )
+  done
+fi
+
 # dash comments for haskell
 ls *.hs >/dev/null 2>&1
 if [ $? = 0 ]
@@ -219,14 +256,16 @@ then
 fi
 
 # percent comments for TeX
-ls *.tex >/dev/null 2>&1
+ls *.tex >/dev/null 2>&1 ||
+ls *.cls >/dev/null 2>&1 ||
+ls *.sty >/dev/null 2>&1
 if [ $? = 0 ]
 then
   echo '1,$ s=^=% =' > $PTMP
   sed -f $PTMP $CFILE > $CTMP
   sed -f $PTMP $LFILE > $LTMP
   sed -f $PTMP $SCFILE > $SCTMP
-  ls *.tex |
+  ls *.tex *.cls *.sty 2>/dev/null |
   while read F
   do
     sed '1 !d; s/^% //; s/ .*$//' < $F | (
@@ -367,7 +406,7 @@ then
   done
 fi
 
-# sharp comments at line 2 for shell script or nickle
+# sharp comments at line 2 for shell script, nickle, etc
 for suff in sh awk 5c rb pl
 do
   ls *.$suff >/dev/null 2>&1
